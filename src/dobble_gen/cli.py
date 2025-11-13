f825315e4aff0ba825d2cecef616f64b2b7aada7
Baptiste Gouhoury <gouhoury.baptiste@gmail.com>
2025-11-13 23:55:19 +0100

    fix: correct gen algorythm

---
+++ src/dobble_gen/cli.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+import sys
+
+import typer
+from rich import print
+from dobble_gen import check, generator
+from dobble_gen.config import (
+    Config,
+    DEFAULT_IMAGES_DIR,
+    DEFAULT_OUTPUT_DIR,
+    DEFAULT_SYMBOLS_PER_CARD,
+    DEFAULT_CARD_DIAMETER_CM,
+    DEFAULT_DPI,
+)
+
+app = typer.Typer()
+
+
+@app.command()
+def run(
+    images_dir: str = DEFAULT_IMAGES_DIR,
+    output_dir: str = DEFAULT_OUTPUT_DIR,
+    symbols_per_card: int = DEFAULT_SYMBOLS_PER_CARD,
+    card_diameter_cm: int = DEFAULT_CARD_DIAMETER_CM,
+    dpi: int = DEFAULT_DPI,
+) -> None:
+    if not check.is_prime(symbols_per_card - 1):
+        print("symbol per card must be a prime number + 1")
+        sys.exit(1)
+    cfg = Config(images_dir, output_dir, symbols_per_card, card_diameter_cm, dpi)
+    generator.run(cfg)
+
+
+@app.command()
+def create_output_dir(
+    images_dir: str = DEFAULT_IMAGES_DIR,
+    output_dir: str = DEFAULT_OUTPUT_DIR,
+) -> None:
+    cfg = Config(images_dir, output_dir)
+    cfg.create_output_dir()
+    print(f"{output_dir} directory created.")
+
+
+def main():
+    app()
---
+++ src/dobble_gen/config.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import os
+
+DEFAULT_IMAGES_DIR = "images"
+DEFAULT_OUTPUT_DIR = "output"
+DEFAULT_SYMBOLS_PER_CARD = 8
+DEFAULT_CARD_DIAMETER_CM = 8
+DEFAULT_DPI = 300
+
+
+def nb_cards(symbols_per_card: int) -> int:
+    return (symbols_per_card**2) - symbols_per_card + 1
+
+
+class Config:
+    def __init__(
+        self,
+        image_dir: str = DEFAULT_IMAGES_DIR,
+        output_dir: str = DEFAULT_OUTPUT_DIR,
+        symbols_per_card: int = DEFAULT_SYMBOLS_PER_CARD,
+        card_diameter_cm: int = DEFAULT_CARD_DIAMETER_CM,
+        dpi: int = DEFAULT_DPI,
+    ):
+        self.image_dir = image_dir
+        self.output_dir = output_dir
+        self.symbols_per_card = symbols_per_card
+        self.card_diameter_cm = card_diameter_cm
+        self.card_dir = os.path.join(output_dir, "cards")
+        self.num_cards = nb_cards(symbols_per_card)
+        self.card_diameter_px = int(card_diameter_cm / 2.54 * dpi)
+
+    def create_output_dir(self) -> None:
+        os.makedirs(self.output_dir, exist_ok=True)
+        os.makedirs(self.card_dir, exist_ok=True)
